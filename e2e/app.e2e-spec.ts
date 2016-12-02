46b1bb07a341eab0f6833b3d14e3a56fbca09b21
Daryl <daryl@lyraddigital.com>
2016-12-02 20:21:16 +1100

    First commit to add a blank skeleton webpack app before making changes

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2tutorialPage } from './app.po';
+
+describe('angular2tutorial App', function() {
+  let page: Angular2tutorialPage;
+
+  beforeEach(() => {
+    page = new Angular2tutorialPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
