9ca4b60babf7e310cf9fd5aaf42e05bf13209b93
Daryl <daryl@lyraddigital.com>
2016-12-03 17:49:10 +1100

    Some tutorial 6 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+             <h1>{{title}}</h1>
+             <router-outlet></router-outlet>
+            `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'I am awesome';
+  title = 'Basketball Stats';
 }
