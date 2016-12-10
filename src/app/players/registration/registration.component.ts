ab013248381c95762368e30f2ecbb9d9d31bd58b
Daryl <daryl@lyraddigital.com>
2016-12-10 15:49:06 +1100

    Moved the validation to the code and added some more descriptive text

--- src/app/players/registration/registration.component.ts
+++ src/app/players/registration/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { RegistrationViewModel } from './registrationViewModel';
 import { TeamLookup } from '../../shared/lookup/team-lookup';
@@ -11,10 +12,57 @@ import { TeamLookupService } from '../../shared/lookup/team-lookup.service';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm: NgForm;
+  @ViewChild('registrationForm') currentForm: NgForm;
   registrationViewModel: RegistrationViewModel;
   teamLookupList: TeamLookup[];
   injuryLookupList: string[];
   injuryFieldList: string[];
+
+  private formErrors = {
+    'firstName': '',
+    'lastName': '',
+    'email': '',
+    'dob': '',
+    'teamId': '',
+    'heightInFeet': '',
+    'heightInInches': '',
+    'weight': '',
+    'position': ''
+  };
+
+  private validationMessages = {
+    'firstName': {
+      'required': 'First name is required'
+    },
+    'lastName': {
+      'required': 'Last name is required'
+    },
+    'email': {
+      'required': 'Email is required'
+    },
+    'dob': {
+      'required': 'DOB is required'
+    },
+    'teamId': {
+      'required': 'Team is required'
+    },
+    'heightInFeet': {
+      'required': 'Height in feet is required'
+    },
+    'heightInInches': {
+      'required': 'Height in inches is required'
+    },
+    'weight': {
+      'required': 'Weight in pounds is required'
+    },
+    'position': {
+      'required': 'Position is required'
+    },
+    'password': {
+      'required': 'Password is required'
+    }
+  };
   
   constructor(
     private injuryLookupService: InjuryLookupService,
@@ -27,6 +75,38 @@ export class RegistrationComponent implements OnInit {
     this.teamLookupList = this.teamLookupService.getTeamsLookup();
     this.injuryLookupList = this.injuryLookupService.getInjuriesLookup();
     this.initialiseInjuryFieldList();
+  }
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    if (this.currentForm === this.registrationForm) { return; }
+    this.registrationForm = this.currentForm;
+    
+    if (this.registrationForm) {
+      this.registrationForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    }
+  }
+
+  private onValueChanged(data?: any) {
+    if (!this.registrationForm) { return; }
+    const form = this.registrationForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+  
+      if (control && control.dirty && control.invalid) {
+        const messages = this.validationMessages[field];
+        
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   private initialiseInjuryFieldList() {
